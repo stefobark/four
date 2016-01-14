d996881f7d3d842457d8bd429c59a7fe016f9857
stefobark <steve@sphinxsearch.com>
2016-01-14 05:41:00 -0800

    chapter 4

---
+++ fourEleven.cpp
@@ -0,0 +1,5 @@
+4.11
+Write an implementation of the set class, with associated iterators using a binary
+search tree. Add to each node a link to the parent node.
+
+interesting. gotta read! and write code!
---
+++ fourSeven.cpp
@@ -0,0 +1,2 @@
+4.7
+gotta think about proving this. should probably go in for tutoring
